cd30d0e6dae6ef64b45dd58208222a534cfb3269
Randeepa123 <158833313+Randeepa123@users.noreply.github.com>
2025-03-28 13:32:23 +0530

    implemented fragments

--- app/src/main/java/com/example/labexam02/MainActivity.kt
+++ app/src/main/java/com/example/labexam02/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.labexam02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,10 @@ class MainActivity : AppCompatActivity() {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root) // Use ViewBinding's root
 
+        if(savedInstanceState==null){
+            replaceFragment(HomeFragment())
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(mainBinding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -30,5 +35,22 @@ class MainActivity : AppCompatActivity() {
         mainBinding.bottomNavigationView.setOnApplyWindowInsetsListener(null)
         mainBinding.bottomNavigationView.setPadding(0, 0, 0, 0)
 
+        mainBinding.bottomNavigationView.setOnItemSelectedListener {item->
+            when(item.itemId){
+                R.id.home_icon -> replaceFragment(HomeFragment())
+                R.id.profile_icon -> replaceFragment(ProfileFragment())
+                R.id.analytics_con -> replaceFragment(AnalyticFragment())
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment): Boolean{
+
+        supportFragmentManager.beginTransaction()
+        .replace(R.id.main,fragment)
+        .commit()
+
+        return true
     }
 }
