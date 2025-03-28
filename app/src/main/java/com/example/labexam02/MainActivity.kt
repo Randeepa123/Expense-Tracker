71fb60777e484743468615e3515aacbcdcc63079
Randeepa123 <158833313+Randeepa123@users.noreply.github.com>
2025-03-28 15:54:39 +0530

    implemented the bottomsheet

--- app/src/main/java/com/example/labexam02/MainActivity.kt
+++ app/src/main/java/com/example/labexam02/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.labexam02
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.Window
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -38,11 +44,14 @@ class MainActivity : AppCompatActivity() {
         mainBinding.bottomNavigationView.setOnItemSelectedListener {item->
             when(item.itemId){
                 R.id.home_icon -> replaceFragment(HomeFragment())
+                R.id.add_icon->showDialog()
                 R.id.profile_icon -> replaceFragment(ProfileFragment())
                 R.id.analytics_con -> replaceFragment(AnalyticFragment())
                 else -> false
             }
         }
+
+
     }
 
     private fun replaceFragment(fragment: Fragment): Boolean{
@@ -52,5 +61,31 @@ class MainActivity : AppCompatActivity() {
         .commit()
 
         return true
+    }
+
+    private fun showDialog(): Boolean{
+        val dialog= Dialog(this)
+        dialog.setContentView(R.layout.add_expense)
+        dialog.show()
+        dialog.window!!.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, 1200)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.attributes.windowAnimations=R.style.DialogAnimation
+        dialog.window!!.setGravity(Gravity.BOTTOM)
+        dialog.setOnDismissListener {
+            restoreBottomNavigationSelection()
+        }
+
+        return true;
+
+    }
+
+    private fun restoreBottomNavigationSelection() {
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.main)
+
+        when (currentFragment) {
+            is HomeFragment -> mainBinding.bottomNavigationView.selectedItemId = R.id.home_icon
+            is ProfileFragment -> mainBinding.bottomNavigationView.selectedItemId = R.id.profile_icon
+            is AnalyticFragment -> mainBinding.bottomNavigationView.selectedItemId = R.id.analytics_con
+        }
     }
 }
