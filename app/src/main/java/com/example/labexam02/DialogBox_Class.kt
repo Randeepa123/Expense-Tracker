26729683f83b2abb6354d89baea55ca0d5c30571
Randeepa123 <158833313+Randeepa123@users.noreply.github.com>
2025-04-21 18:51:25 +0530

    made some changes in UI

--- app/src/main/java/com/example/labexam02/DialogBox_Class.kt
+++ app/src/main/java/com/example/labexam02/DialogBox_Class.kt
@@ -7,10 +7,13 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
 
 class DialogBox_Class:DialogFragment() {
 
@@ -19,16 +22,42 @@ class DialogBox_Class:DialogFragment() {
         val etTitle=view.findViewById<TextView>(R.id.editTitle);
         val etAmount=view.findViewById<TextView>(R.id.editExpenseAmount);
         val etBtnSubmit=view.findViewById<Button>(R.id.buttonSubmit);
+        val selectedCategory=view.findViewById<Spinner>(R.id.btn_cash);
 
+
+
+
+        selectedCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = parent?.getItemAtPosition(position).toString()
+
+                when (selectedItem) {
+                    "Expense" -> replaceAdditemFragment(enterExpensefragment(),false)
+                    "Income" -> replaceAdditemFragment(enterIncomefragment(),true)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Optional: handle case when nothing is selected
+            }
+        }
+
+/*
         etBtnSubmit.setOnClickListener {
             val name = etTitle.text.toString()
             val amount = etAmount.text.toString().toIntOrNull() ?: 0
+
 
             dialog?.dismiss();
             Toast.makeText(requireContext(), "Name: $name, Amount: $amount", Toast.LENGTH_SHORT).show()
 
         }
-
+*/
     }
 
     override fun onCreateView(
@@ -54,4 +83,19 @@ class DialogBox_Class:DialogFragment() {
         super.onDismiss(dialog)
         (activity as? MainActivity)?.restoreBottomNavigationSelection();
     }
+
+
+    private fun replaceAdditemFragment(fragment: Fragment,isIncome:Boolean): Boolean{
+        val shoppingIconSpinner=view?.findViewById<Spinner>(R.id.btn_shopping);
+        childFragmentManager.beginTransaction()
+        .replace(R.id.AmountDisplayFragment,fragment)
+        .commit()
+
+        if(isIncome==true){
+            shoppingIconSpinner?.visibility = View.INVISIBLE
+        }else{
+            shoppingIconSpinner?.visibility = View.VISIBLE
+        }
+        return true}
 }
+
